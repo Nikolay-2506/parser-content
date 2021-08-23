16375d1e5856dce4a3deed39e64b07069075a09c
nikolay <57495749Katy>
2021-08-23 22:47:36 +0700

    создание проекта

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newParserNpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
